27877636c90532e211b31be5406cdd87a80cf457
boxak <boxak@naver.com>
2023-06-09 00:16:40 +0900

    temp commit

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 
 function Login(){
-    const restApiKey = 'ffefc95e6555a1eee5499d4cf824f9ec';
+    const restApiKey = 'ffefc95e6555a1eee5499d4cf824f9ec'; // REST API KEY
     const redirectUrl = 'http://localhost:8080/auth';
     const kakaoUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${restApiKey}&redirect_uri=${redirectUrl}&response_type=code`;
 
@@ -9,7 +9,7 @@ function Login(){
 
     const initKakao = () => {
         if (Kakao && !Kakao.isInitialized()) {
-            Kakao.init("KAKAO_API_KEY");
+            Kakao.init("bd94eba67cf1b53443162a52cd86de50");
         }
     }
 
@@ -20,23 +20,20 @@ function Login(){
     const handleLogin = () => {
         Kakao.Auth.authorize({
             redirectUri : redirectUrl,
-            scope : "profile_nickname, profile_image"
+            scope : "profile_nickname, profile_image, birthday"
         });
     }
 
     return(
         <div className="Login">
-            <div className="greeting">
-                <h2>반가워요!</h2>
-                <p>소중한 사람들과 주고 받은</p>
-                <p>소중한 마음들을 기록하는 앱</p>
-                <p>맘관부에요</p>
-            </div>
             <div className="button-container">
-                <button id="kakao-login-btn"
-                    onClick={handleLogin}>
-                    <img src={`${process.env.PUBLIC_URL}/kakao/ko/kakao_login_large_wide.png`} 
-                        alt="카카오 로그인"></img>
+                <button onClick={handleLogin}>
+                    <img src={`${process.env.PUBLIC_URL}/buttons/KaKao Button.png`} alt='카카오 로그인'/>
+                </button>
+            </div>
+            <div className="login-btn-div">
+                <button>
+                    <img src={`${process.env.PUBLIC_URL}/buttons/Apple Button.png`} alt='애플 로그인'/>
                 </button>
             </div>
         </div>
