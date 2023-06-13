0e0edaed1e433593abf946932200ff9c7c88fc4e
boxak <boxak@naver.com>
2023-06-13 23:36:35 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,24 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signp";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Agreement from './pages/login/Agreement';
+import Main from './pages/main/Main';
+import { Provider } from 'mobx-react';
+import RootStore from './store/RootStore';
 
 function App() {
     return (
-    <div className="App">
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Login />}></Route>
-                <Route path="/signup" element={<Signup />}></Route>
-                <Route path="/agreement" element={<Agreement />}></Route>
-            </Routes>
-        </BrowserRouter>
-    </div>
+    <Provider RootStore={RootStore}>
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />}></Route>
+                    <Route path="/signup" element={<Signup />}></Route>
+                    <Route path="/agreement" element={<Agreement />}></Route>
+                    <Route path="/main" element={<Main/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    </Provider>
     );
 }
 
