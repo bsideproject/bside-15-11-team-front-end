20c8091fd426a8dd2ce17bd1d33e2967d028f24d
boxak <boxak@naver.com>
2023-06-14 21:24:30 +0900

    소셜로그인-프론트에서 처리하도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,26 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signp";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Agreement from './pages/login/Agreement';
-// import Main from './pages/main/Main';
-// import { Provider } from 'mobx-react';
-// import RootStore from './store/RootStore';
+import Main from './pages/main/Main';
+import { Provider } from 'mobx-react';
+import RootStore from './store/RootStore';
+import Auth from './pages/login/Auth';
 
 function App() {
     return (
-    // <Provider RootStore={RootStore}>
+    <Provider RootStore={RootStore}>
         <div className="App">
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Login />}></Route>
                     <Route path="/signup" element={<Signup />}></Route>
                     <Route path="/agreement" element={<Agreement />}></Route>
-                    {/* <Route path="/main" element={<Main/>}></Route> */}
+                    <Route path="/main" element={<Main/>}></Route>
+                    <Route path="/auth" element={<Auth/>}></Route>
                 </Routes>
             </BrowserRouter>
         </div>
-    // </Provider>
+    </Provider>
     );
 }
 
