c4fa969605c950471ee6dbda4f939f57c23da8fd
boxak <boxak@naver.com>
2023-07-15 20:27:50 +0900

    로그인 및 약관동의 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
-import Login from "./pages/login/Login";
-import Signup from "./pages/signup/Signp";
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Agreement from './pages/login/Agreement';
 import Main from './pages/main/Main';
 import { Provider } from 'mobx-react';
 import RootStore from './store/RootStore';
-import KakaoAuth from './pages/login/KakaoAuth';
 import Friend from "./pages/friend/Friend";
 import Mind from './pages/mind/Mind';
 import Detail from "./pages/detail/Detail";
 
 function App() {
+
+    const [isMessageReceived, setMessageReceived] = useState<boolean>(false);
+
+    useEffect(() => {
+
+        const handleMessage = (event : any) => {
+            if(!isMessageReceived) {
+
+                setMessageReceived(true);
+                window.removeEventListener('message', handleMessage);
+
+                const data = JSON.parse(event.data);
+
+                RootStore.userStore.setJwtKey(data.jwtKey);
+                RootStore.userStore.setServiceUserId(data.userData.sequence);
+            }
+        };
+
+        window.addEventListener('message', handleMessage);
+
+        return () => {
+            window.removeEventListener('message', handleMessage);
+        }
+
+    }, [isMessageReceived]);      
+
     return (
     <Provider RootStore={RootStore}>
         <div className="App">
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Login />}></Route>
-                    <Route path="/page/signup" element={<Signup />}></Route>
-                    <Route path="/page/agreement" element={<Agreement />}></Route>
-                    <Route path="/page/main" element={<Main />}></Route>
+                    <Route path='/' element={<Main />}></Route>
+                    <Route path='/page/main' element={<Main />}></Route>
                     <Route path="/page/friend" element={<Friend />}></Route>
-                    <Route path="/page/auth/kakao" element={<KakaoAuth/>}></Route>
                     <Route path="/page/relationship" element={<Mind />}></Route>
                     <Route path="/page/detail" element={<Detail />}></Route>
-
                 </Routes>
             </BrowserRouter>
         </div>
